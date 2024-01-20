c88bd36f63b91ca4a63997bfc2f1fd167931a531
João <jvictorml06@gmail.com>
2024-01-20 16:39:15 -0300

    correção migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,6 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
             $table->id('user_id');
             $table->string('name');
             $table->string('email')->unique();
